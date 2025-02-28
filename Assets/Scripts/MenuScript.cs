216d42a489c9d30fbfe380627377688725240341
OcrabitGames <ocrabit@outlook.com>
2025-02-28 15:36:01 -0600

    MainMenu Scene and Transition
    Created a landing menu page and a button to load into the gameplay.

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public void GoToGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("gameplay");
+    }
+}
